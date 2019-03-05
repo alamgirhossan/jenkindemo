22ca2074651d0740625dc2230b17529830dbca85
alamgirhossan <alamgir8@yahoo.com>
2019-03-05 14:16:02 -0500

    My first commit

---
+++ GitDemo/src/test/java/MavenParactice/GitDemo/GitDemo1.java
@@ -0,0 +1,12 @@
+package MavenParactice.GitDemo;
+
+import org.testng.annotations.Test;
+
+public class GitDemo1 {
+	
+	@Test
+	public void testGit() {
+		System.out.println("this is my firs git program");
+	}
+
+}
